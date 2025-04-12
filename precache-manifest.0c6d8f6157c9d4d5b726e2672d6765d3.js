3ddc79475cc6fad73930955b7fb32ff18f739a53
sgaredew <sgaredew@gmail.com>
2025-04-12 11:17:51 -0500

    Updates

---
+++ precache-manifest.0c6d8f6157c9d4d5b726e2672d6765d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "419fe1e6e5b3f35200edb6d9805b93bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f3c89fc4568db6b4278b",
+    "url": "/static/css/main.3e1abdde.chunk.css"
+  },
+  {
+    "revision": "e13fa16b3adcb733a610",
+    "url": "/static/js/2.83d5cc1d.chunk.js"
+  },
+  {
+    "revision": "4cd4bb82c3efd9af5440b12fb6d92f3f",
+    "url": "/static/js/2.83d5cc1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3c89fc4568db6b4278b",
+    "url": "/static/js/main.3f478198.chunk.js"
+  },
+  {
+    "revision": "df31576aa105eaa2d16d",
+    "url": "/static/js/runtime-main.05aeeedb.js"
+  }
+]);
